c8d53af83e958c60afa92b028622916a0076f8f0
Claire Walker <avid.gamer81@gmail.com>
2020-12-07 15:23:51 +0000

    adding error checking for valid email

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ function showSuccess(input) {
     formControl.className = "form-control success";
 }
 
+//check email is valid
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // Event Listeners
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -30,6 +36,8 @@ form.addEventListener("submit", (e) => {
 
     if(email.value === "") {
         showError(email, "Email is required");
+    } else if(!isValidEmail(email.value)) {
+        showError(email, "Email is not valid");
     } else {
         showSuccess(email);
     }
